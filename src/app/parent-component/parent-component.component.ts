3b6e9dfd72fc1559b2ac415e0619137d79ae934c
Alfred Kwak <williamlambert88@gmail.com>
2022-01-07 15:51:25 +0100

    added 3rd component

---
+++ src/app/parent-component/parent-component.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-component',
+  templateUrl: './parent-component.component.html',
+  styleUrls: ['./parent-component.component.scss'],
+})
+export class ParentComponentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
